cda9a411ae15e7e70324ee3d1afa12fa0ce6a405
ganzes <charczuk.arkadiusz@gmail.com>
2019-08-22 03:26:38 +0200

    GroupController - wstępna implementacja endpointów

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,30 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GenericEntity> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createGroup")
+    public void createGroup(GenericEntity groupDto){
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroupById")
+    public GenericEntity getGroupById (Long productGroupById){
+        return new GenericEntity("Products Group 01");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroupById")
+    public GenericEntity updateGroupById(GenericEntity groupDto){
+        return new GenericEntity();
+    }
 }
