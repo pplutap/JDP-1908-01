8e89c90d45bc474e228b7a8af5f533c60456640d
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-08-23 19:49:09 +0200

    JDP190801-3 - CartController - wstępna implementacja endpointów - update

--- src/main/java/com/kodilla/ecommercee/controllers/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controllers/CartController.java
@@ -3,8 +3,6 @@ package com.kodilla.ecommercee.controllers;
 import com.kodilla.ecommercee.GenericEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -12,27 +10,24 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/v1/cart")
 public class CartController {
 
-    @RequestMapping(method = RequestMethod.POST,value = "createCart", consumes = APPLICATION_JSON_VALUE)
-    public void createCart(@RequestBody GenericEntity cartDto){
+    @RequestMapping(method = RequestMethod.GET, value = "getCart")
+    public GenericEntity getCartById(@RequestParam Long cartId) {
+        return new GenericEntity();
     }
 
-    @RequestMapping(method = RequestMethod.GET,value = "getProducts")
-    public List<GenericEntity> getProductsFromCart(){
-        return new ArrayList<>();
+    @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = APPLICATION_JSON_VALUE)
+    public void createCart(@RequestBody GenericEntity cartDto) {
     }
 
-    @RequestMapping(method = RequestMethod.POST,value = "addProducts")
-    public void addProductsToCart(@RequestBody List<GenericEntity> productDto){
+    @RequestMapping(method = RequestMethod.PUT, value = "updateCart")
+    public GenericEntity updateCart(@RequestBody GenericEntity cartDto) {
+        return new GenericEntity();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteCart")
+    public void deleteCartById(@RequestParam Long cartId) {
 
     }
 
-    @RequestMapping(method = RequestMethod.DELETE,value = "deleteProduct")
-    public void deleteProductByName(@RequestParam String name){
-
-    }
-
-    @RequestMapping(method = RequestMethod.POST,value = "createOrder")
-    public void createOrder(@RequestBody GenericEntity orderDto){
-
-    }
 }
+
