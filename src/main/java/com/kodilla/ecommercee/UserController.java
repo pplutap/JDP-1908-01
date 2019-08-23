8111702d4cac732b18daf5448846a784c5e9bcac
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-08-23 21:00:34 +0200

    task JDP190801-5 v1

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.exceptions.UserNotFoundException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/user")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getUser")
+    public GenericEntity getUser(@RequestParam Long id) throws UserNotFoundException {
+        return null;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    public void blockUser(@RequestParam Long id) throws UserNotFoundException {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getKey")
+    public String getKey() {
+        return "";
+    }
 }
