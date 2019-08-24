c5f5f7bf60732208517f22016f5a83bc26496ebe
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-08-24 12:31:25 +0200

    JDP190801-3 - CartController - wstępna implementacja endpointów - update v3

--- src/main/java/com/kodilla/ecommercee/controllers/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controllers/CartController.java
@@ -3,6 +3,7 @@ package com.kodilla.ecommercee.controllers;
 import com.kodilla.ecommercee.GenericEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -10,18 +11,18 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/v1/cart")
 public class CartController {
 
+    @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = APPLICATION_JSON_VALUE)
+    public void createCart(@RequestBody GenericEntity cartDto) {
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "getCart")
     public GenericEntity getCartById(@RequestParam Long cartId) {
         return new GenericEntity();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createCart", consumes = APPLICATION_JSON_VALUE)
-    public void createCart(@RequestBody GenericEntity cartDto) {
-    }
+    @RequestMapping(method = RequestMethod.POST, value = "addProducts")
+    public void addProductsToCart(@RequestBody List<GenericEntity> productDto, @RequestParam Long cartId) {
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateCart")
-    public GenericEntity updateCart(@RequestBody GenericEntity cartDto) {
-        return new GenericEntity();
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteCart")
@@ -29,5 +30,8 @@ public class CartController {
 
     }
 
-}
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public void createOrder(@RequestParam Long cartId) {
 
+    }
+}
