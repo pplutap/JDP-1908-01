4db17bccb34f8486d14e59bb9154e2c1351aeb98
KingKara <kinga.kara.93@gmail.com>
2019-08-29 21:55:54 +0200

    JDP190801-14 v1 Cart entity implementation

---
+++ src/main/java/com/kodilla/ecommercee/domains/Cart.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.domains;
+
+import com.kodilla.ecommercee.GenericEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CARTS")
+public class Cart {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CART_ID", unique = true)
+    private Long cartId;
+
+    @Getter
+    @OneToOne
+    @JoinColumn(name = "USER_ID")
+    private GenericEntity user;
+
+    @Getter
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "carts")
+    private List<Product> products;
+}
