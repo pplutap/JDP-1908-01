0a077424617576d5309809b152dd98f1f3bde4d6
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-08-29 21:04:09 +0200

    JDP190801-11 v1

---
+++ src/main/java/com/kodilla/ecommercee/domains/Order.java
@@ -0,0 +1,31 @@
+package com.kodilla.ecommercee.domains;
+
+import com.kodilla.ecommercee.GenericEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@NoArgsConstructor
+@Entity
+@Table(name= "ORDERS")
+public class Order {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ORDER_ID", unique = true)
+    private Long orderId;
+
+    @Getter
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private GenericEntity user;
+    //TODO change GenericEntity to User entity, when it's done
+
+    @Getter
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "orders")
+    private List<Product> products;
+}
