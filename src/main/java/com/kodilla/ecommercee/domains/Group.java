572b46d67cc6941963666395146788f665d8d534
ganzes <charczuk.arkadiusz@gmail.com>
2019-08-31 01:07:44 +0200

    JDP190801-13 - Encja Group - implementacja

---
+++ src/main/java/com/kodilla/ecommercee/domains/Group.java
@@ -0,0 +1,37 @@
+package com.kodilla.ecommercee.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "GROUP")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Column(name = "GROUP_ID", unique = true)
+    private Long groupId;
+
+    @Column(name = "GROUP_NAME")
+    private String groupName;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY)
+    public List<Product> products = new ArrayList<>();
+
+}
