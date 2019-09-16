7bf59fbc2c935c393b037264b449f6202b25883c
PawelDemichowicz <pawel.demichowicz@interia.pl>
2019-09-16 09:35:21 +0200

    JDP190801-23 - refactor CartNotFoundException

--- src/main/java/com/kodilla/ecommercee/exceptions/CartNotFoundException.java
+++ src/main/java/com/kodilla/ecommercee/exceptions/CartNotFoundException.java
@@ -3,6 +3,6 @@ package com.kodilla.ecommercee.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Cart not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "Cart not found!")
 public class CartNotFoundException extends Exception{
 }
