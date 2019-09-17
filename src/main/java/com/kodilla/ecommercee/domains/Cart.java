b8ec897fcf394316fded3afb5b1c5c3ab3a71721
Kuba <kuba@MacBook-Air-Kuba.local>
2019-09-17 13:45:27 +0200

    JDP190801-19 testy encji Cart

--- src/main/java/com/kodilla/ecommercee/domains/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domains/Cart.java
@@ -3,6 +3,7 @@ package com.kodilla.ecommercee.domains;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,10 +16,12 @@ public class Cart {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
     @Column(name = "CART_ID", unique = true)
     private Long cartId;
 
     @Getter
+    @Setter
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "USER_ID")
     private User user;
